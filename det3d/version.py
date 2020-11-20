709883e918893e02af4bc8870052f9ec65d9e201
shovington <samuel.hovington@usherbrooke.ca>
2020-11-20 15:26:05 -0700

    change the loading script to load only car and pedestrian weights and comment config file to create anchors only for pedestrians and cars

--- det3d/version.py
+++ det3d/version.py
@@ -1,4 +1,4 @@
 # GENERATED VERSION FILE
-# TIME: Fri Jun 19 17:20:56 2020
-__version__ = '1.0.rc0+2f20224'
+# TIME: Wed Oct 28 08:52:06 2020
+__version__ = '1.0.rc0+ae5a3ae'
 short_version = '1.0.rc0'
